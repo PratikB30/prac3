aafcb544c1b36db63d26b19f15969524a9c7839f
PratikB30 <pratikbhat73@gmail.com>
2024-04-30 21:11:41 +0530

    first commit

---
+++ AddNumbers.java
@@ -0,0 +1,11 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        
+        int num1 = 4;
+        int num2 = 10;
+
+        int sum = num1 + num2;
+
+        System.out.println("Sum:" + sum);
+    }
+}
